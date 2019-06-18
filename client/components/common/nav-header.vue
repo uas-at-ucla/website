332eaac60232e336eedcd6a3ea544cf28011e235
Comran Morshed <comranmorsh@gmail.com>
2019-06-17 17:06:43 -0700

    Restore admin buttons to mobile.

--- client/components/common/nav-header.vue
+++ client/components/common/nav-header.vue
@@ -45,7 +45,7 @@
             icon
             )
             v-icon(color='grey') search
-          v-menu(open-on-hover, offset-y, bottom, left, min-width='250', transition='slide-y-transition', v-if="!$vuetify.breakpoint.smAndDown")
+          v-menu(open-on-hover, offset-y, bottom, left, min-width='250', transition='slide-y-transition')
             v-toolbar-side-icon.btn-animate-app(slot='activator')
               v-icon(color='grey') view_module
             v-list(dense, :light='!$vuetify.dark', :dark='$vuetify.dark', :class='$vuetify.dark ? `grey darken-4` : ``').py-0
@@ -82,7 +82,7 @@
                 v-list-tile-avatar: v-icon(color='grey lighten-2') burst_mode
                 v-list-tile-content.grey--text.text--ligten-2 {{$t('common:header.imagesFiles')}}
 
-          v-tooltip(bottom, v-if='isAuthenticated && isAdmin && !$vuetify.breakpoint.smAndDown')
+          v-tooltip(bottom, v-if='isAuthenticated && isAdmin')
             v-btn.btn-animate-rotate(icon, href='/a', slot='activator')
               v-icon(color='grey') settings
             span {{$t('common:header.admin')}}
